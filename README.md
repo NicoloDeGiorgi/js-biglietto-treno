e527158841d29e9778925da86ed502aba2b179b9
Nicolò <nico.2699@icloud.com>
2024-05-08 21:01:59 +0200

    add;  js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 
 1// definire costo x km
-2// creare due variabili (prezzo del km) e (etaPassseggero)
+2// creare due prompt (prezzo del km) e (etaPassseggero)
 
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
-'use strict';
+'use strict'
 
-// costo del km
-let costoKm = '0.21';
-console.log(costoKm)
+//1. costo km
+
+let costoKM = 0.21;
+console.log(costoKM);
+
+//2. numeroKM da percorrere 
+
+let numeroKM = parseInt(prompt('Quanti chilometri vuoi percorrere?', 'es. 10'));
+console.log(numeroKM);
+
+//3. numero eta
+
+let etaUtente = parseInt(prompt('Quanti anni hai?', 'es. 20'));
+console.log(etaUtente);
+
+//4. calcola prezzo (numeroKM * costoKM)
+
+let prezzoIntero = (numeroKM * costoKM);
+console.log(prezzoIntero);
+
+// 5. definizione variabili con sconto
+
+let prezzoScontato1 = parseInt(prezzoIntero - (prezzoIntero * 0.2));
+let prezzoScontato2 = parseInt(prezzoIntero - (prezzoIntero * 0.4));
+
+/*6. SE etaUtente < 18 mostra prezzo - 20%  
+   altrimenti SE etaUtente ≥ 65 mostra prezzo - 40% 
+   altrimenti mostra prezzo */
+
+if (etaUtente < 18) {
+    prezzoScontato1;
+    console.log(prezzoScontato1);
+    alert('Sei minorenne e hai diritto ad uno sconto del 20%');
+    document.getElementById("prezzofinale").innerHTML =
+        `Il tuo biglietto costa: ${prezzoScontato1} €`;
+} else if (etaUtente >= 65) {
+    prezzoScontato2;
+    console.log(prezzoScontato2)
+    alert('Sei over 65 e hai diritto ad uno sconto del 40%');
+    document.getElementById("prezzofinale").innerHTML =
+        `Il tuo biglietto costa: ${prezzoScontato2} €`;
+} else {
+    prezzoIntero;
+    alert('Paghi il prezzo pieno');
+    document.getElementById("prezzofinale").innerHTML =
+        `Il tuo biglietto costa: ${prezzoIntero} €`;
