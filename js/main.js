d6b56fbce7d8c3f553068901dffcb5bf486a0765
Nicolò <nico.2699@icloud.com>
2024-05-11 15:56:10 +0200

    add: complete-ex

--- js/main.js
+++ js/main.js
@@ -1,48 +1,42 @@
 'use strict'
 
+// Recupero gli elementi dalla pagina
+const myElement = document.getElementById('prezzofinale');
 //1. costo km
 
-let costoKM = 0.21;
+const costoKM = 0.21;
 console.log(costoKM);
 
 //2. numeroKM da percorrere 
 
-let numeroKM = parseInt(prompt('Quanti chilometri vuoi percorrere?', 'es. 10'));
+const numeroKM = parseInt(prompt('Quanti chilometri vuoi percorrere?', 'es. 10'));
 console.log(numeroKM);
 
 //3. numero eta
 
-let etaUtente = parseInt(prompt('Quanti anni hai?', 'es. 20'));
+const etaUtente = parseInt(prompt('Quanti anni hai?', 'es. 20'));
 console.log(etaUtente);
 
 //4. calcola prezzo (numeroKM * costoKM)
 
-let prezzoIntero = (numeroKM * costoKM);
+const prezzoIntero = (numeroKM * costoKM);
 console.log(prezzoIntero);
 
 // 5. definizione variabili con sconto
 
-let prezzoScontato1 = parseInt(prezzoIntero - (prezzoIntero * 0.2));
-let prezzoScontato2 = parseInt(prezzoIntero - (prezzoIntero * 0.4));
 
 /*6. SE etaUtente < 18 mostra prezzo - 20%  
-   altrimenti SE etaUtente ≥ 65 mostra prezzo - 40% 
-   altrimenti mostra prezzo */
+altrimenti SE etaUtente ≥ 65 mostra prezzo - 40% 
+altrimenti mostra prezzo */
+
+let prezzoFinale = prezzoIntero;
 
 if (etaUtente < 18) {
-    prezzoScontato1;
-    console.log(prezzoScontato1);
-    alert('Sei minorenne e hai diritto ad uno sconto del 20%');
-    document.getElementById("prezzofinale").innerHTML =
-        `Il tuo biglietto costa: ${prezzoScontato1} €`;
+    const prezzoScontato1 = parseInt(prezzoIntero - (prezzoIntero * 0.2));
+    prezzoFinale = prezzoScontato1;
 } else if (etaUtente >= 65) {
-    prezzoScontato2;
-    console.log(prezzoScontato2)
-    alert('Sei over 65 e hai diritto ad uno sconto del 40%');
-    document.getElementById("prezzofinale").innerHTML =
-        `Il tuo biglietto costa: ${prezzoScontato2} €`;
-} else {
-    prezzoIntero;
-    alert('Paghi il prezzo pieno');
-    document.getElementById("prezzofinale").innerHTML =
-        `Il tuo biglietto costa: ${prezzoIntero} €`;
+    prezzoFinale = prezzoScontato2;
+    const prezzoScontato2 = parseInt(prezzoIntero - (prezzoIntero * 0.4));
+}
+
+myElement.innerHTML = `Il tuo biglietto costa: ${prezzoFinale} €`;
